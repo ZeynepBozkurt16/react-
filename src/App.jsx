bd40ca2876bc12a7e89a305bc32addd3e96717c8
Zeynep Bozkurt <116113746+ZeynepBozkurt16@users.noreply.github.com>
2024-06-11 18:01:47 +0300

    resimler props olarak geçildi

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-<<<<<<< HEAD
 import Course from './Course'
-=======
->>>>>>> 32384f37abc3795c9f7a57212259aab1c43afebe
+
 
 function App() {
 
@@ -13,13 +11,12 @@ function App() {
   return (
     <>
 
-<<<<<<< HEAD
+
       <Course title="angular" description="Lorem ipsum dolor sit amet."> </Course>
       <Course title="bootstrap 5" description="Lorem ipsum dolor sit amet"></Course>
       <Course title="web" description="Lorem ipsum dolor sit amet"></Course>
       <Course title="frontend" description="lorem ispum"> </Course>
-=======
->>>>>>> 32384f37abc3795c9f7a57212259aab1c43afebe
+
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,10 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<<<<<<< HEAD
+
   <>
     <App />
-  </>,
-=======
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
->>>>>>> 32384f37abc3795c9f7a57212259aab1c43afebe
+  </>
+
+
 )
