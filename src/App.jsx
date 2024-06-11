2e10c3f3253bfbee4af2e87ffa8bae89e2ee9ea0
Zeynep Bozkurt <116113746+ZeynepBozkurt16@users.noreply.github.com>
2024-06-11 15:38:22 +0300

    component içinde props kullanıldı

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Course from './Course'
+
+function App() {
+
+
+  return (
+    <>
+
+      <Course title="angular" description="Lorem ipsum dolor sit amet."> </Course>
+      <Course title="bootstrap 5" description="Lorem ipsum dolor sit amet"></Course>
+      <Course title="web" description="Lorem ipsum dolor sit amet"></Course>
+      <Course title="frontend" description="lorem ispum"> </Course>
+    </>
+  )
+}
+
+export default App
---
+++ src/Course.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Course({ title, description }) {
+    // console.log(props)
+    // // 2.kullanım
+    // const title = props.title
+    // const description = props.description
+    // 3.kullanım
+    // const { title, description } = props;
+    //! 4.kullanımda isimleri aynı olmalı
+
+    return (
+        <div>
+            {/* 1.kullanım */}
+            {/* <div>{props.title}</div>
+            <div>{props.description}</div> */}
+
+            <div>{title}</div>
+            <div>{description}</div>
+        </div>
+    )
+}
+
+export default Course
